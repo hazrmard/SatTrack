2815709253603f134a901324cbc4fd8a164049e0
hazrmard <ibrahim78786@gmail.com>
2016-01-31 16:15:32 -0600

    Cleaned up JS code. Added bootstrap support.

--- sattrack/interface/script.js
+++ sattrack/interface/script.js
@@ -1,10 +1,24 @@
 ﻿$(document).ready(function() {
 	console.log("Document Loaded");
-	$svg = d3.select("#svg").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-	draw();
-	$lon = 0;
+	setVariables()	
+	drawMap();
+    drawSat();
+	getStatus();
+});
+
+var setVariables = function() {
+    // get the smaller of width/height as the map dimension
+    $width = $(window).height() < $(window).width() ? $(window).height() : $(window).width();
+    $height = $width;
+    console.log($width)
+    
+    // append svg to document to draw on
+    $svg = d3.select("#svg").append("svg")
+    .attr("width", $width)
+    .attr("height", $height);
+    
+    // set initial position and references to table cells
+    $lon = 0;
 	$lat = 0;
 	$az = 0;
 	$alt = 0;
@@ -14,39 +28,29 @@
 	$azilabel = d3.select("#azi");
 	$altlabel = d3.select("#alt");
 	$timelabel = d3.select("#time");
-	getStatus();
-	$svg.selectAll("circle")
-		.data([[$lon, $lat]]).enter()
-		.append("circle")
-		.attr("cx", function (d) { return projection(d)[0]; })
-		.attr("cy", function (d) { return projection(d)[1]; })
-		.attr("r", "8px")
-		.attr("fill", "red")
-		.attr("class", "satellite");
-});
-
-var width = $(window).height(),
-    height = $(window).height();
-	console.log(width)
-
-var projection = d3.geo.orthographic()
-    .scale(300)
-    .translate([width / 2, height / 2])
+    
+    // set up projection as a globe
+    $projection = d3.geo.orthographic()
+    .scale($width * 0.459)  // experimentally determined factor
+    .translate([$width / 2, $height / 2])
     .clipAngle(90)
-    .precision(.1);
+    .precision(1);
+    
+    // set up path (to draw map with)
+    $path = d3.geo.path()
+        .projection($projection);
+    
+}
 
-var path = d3.geo.path()
-    .projection(projection);
+var drawMap = function() {
 
-var graticule = d3.geo.graticule();
-
-var draw = function() {
+    var graticule = d3.geo.graticule();
 	
 	
 	$svg.append("defs").append("path")
 		.datum({type: "Sphere"})
 		.attr("id", "sphere")
-		.attr("d", path);
+		.attr("d", $path);
 
 	$svg.append("use")
 		.attr("class", "stroke")
@@ -59,7 +63,7 @@ var draw = function() {
 	$svg.append("path")
 		.datum(graticule)
 		.attr("class", "graticule")
-		.attr("d", path);
+		.attr("d", $path);
 
 	d3.json("world-50m.json", function(error, world) {
 	  if (error) throw error;
@@ -68,16 +72,27 @@ var draw = function() {
 	 $svg.insert("path", ".graticule")
 		  .datum(topojson.feature(world, world.objects.land))
 		  .attr("class", "land")
-		  .attr("d", path);
+		  .attr("d", $path);
 
 	  $svg.insert("path", ".graticule")
 		  .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
 		  .attr("class", "boundary")
-		  .attr("d", path);
+		  .attr("d", $path);
 	});
 
-	d3.select(self.frameElement).style("height", height + "px");
+	d3.select(self.frameElement).style("height", $height + "px");
 };
+
+var drawSat = function() {
+    $svg.selectAll("circle")
+		.data([[$lon, $lat]]).enter()
+		.append("circle")
+		.attr("cx", function (d) { return $projection(d)[0]; })
+		.attr("cy", function (d) { return $projection(d)[1]; })
+		.attr("r", "8px")
+		.attr("fill", "red")
+		.attr("class", "satellite");
+}
 
 function getStatus(){
 	$.getJSON(window.location.href + '?status', function(data) {
@@ -99,12 +114,12 @@ function getStatus(){
 }
 
 function rotateProjection(lat, lon) {
-		projection.rotate([-lon, -lat]);
-		$svg.selectAll("path").attr("d", path);
+		$projection.rotate([-lon, -lat]);
+		$svg.selectAll("path").attr("d", $path);
 }
 
 function plotPoints(lat, lon) {
 	$svg.selectAll(".satellite")
-		.attr("cx", function () { return projection([lon, lat])[0]; })
-		.attr("cy", function () { return projection([lon, lat])[1]; })	;
+		.attr("cx", function () { return $projection([lon, lat])[0]; })
+		.attr("cy", function () { return $projection([lon, lat])[1]; })	;
 }
