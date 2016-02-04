490de1af73f9f659922f0f67ab4380300d93504b
hazrmard <ibrahim78786@gmail.com>
2016-02-04 00:11:47 -0600

    added motor specific angle mapping, cleaned up example funcs.

--- sandbox.py
+++ sandbox.py
@@ -5,23 +5,17 @@ import time
 
 sattrack.MOTOR_DEBUG_MODE = True
 
+# Motor specific configuration
+ALTMAP = lambda x: 15 + (105-15) * x / 90      # mapping 0-90 to 20-95
+AZMAP = lambda x: 47 + (143-47) * x / 360     #  mapping 0-360 to 47-143
 
 def test(tlepath='fox1.tle'):
     s = SatTrack()
     s.set_location()
     s.load_tle(tlepath)
-    #s.get_tle(40967)
     print s.next_pass()
     s.begin_computing(interval=1, trace=0)  # trace > 0 speeds up by a factor of trace / interval (otherwise realtime)
-    #s.connect_servos(minrange=(10, 10), maxrange=(170, 170))
-    #s.begin_tracking()
-    #s.visualize()
     s.show_position()
-    # print 'Front end server set up...'
-    # i = input('Exit? ... ')
-    # if i =='y':
-    #     s.server.stop_server()
-    # print 'server closed'
     return s
     
 def test_web(tlepath='fox1.tle'):
@@ -38,13 +32,15 @@ def test_tracking(name='AO-85', port='COM3'):
     s.set_location()
     s.get_tle(name)
     s.begin_computing(interval=1, trace=10)
-    s.connect_servos(port, minrange=(-90, 0), maxrange=(90, 360))
+    s.connect_servos(port, minrange=(0, 0), maxrange=(90, 360))
+    s.altmotor.map = ALTMAP
+    s.azmotor.map = AZMAP
     s.begin_tracking()
     s.visualize()
     return s
 
 
-def motor_test(num, interval):
+def test_motor(num, interval):
     servos = ServoController(port='COM3', motors=(1,2))
     altmotor, azmotor = servos.motors
     altmotor.map = lambda x: 15 + (105-15) * x / 90      # mapping 0-90 to 20-95
@@ -57,7 +53,9 @@ def motor_test(num, interval):
 # Azimuth motor has limits (47->143) => (-90->270)
     
 
-def q():
+def q(s=None):
+    if s:
+        s.stop()
     try:
         Server().stop_server()
         print "Server closed."
