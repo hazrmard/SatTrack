7733aa466275a0b14204b6a38b07565dfd3ed73c
hazrmard <ibrahim78786@gmail.com>
2016-02-10 11:58:54 -0600

    compacted Adruino sketch, added PW writing function

--- ServoCont/multipleSerialServoControl/multipleSerialServoControl.ino
+++ ServoCont/multipleSerialServoControl/multipleSerialServoControl.ino
@@ -12,12 +12,13 @@
  * Dependencies:
  *   Arduino 0017 or higher
  *     (http://www.arduino.cc/en/Main/Software)
- *   Python servo.py module
- *     (http://principialabs.com/arduino-python-4-axis-servo-control/)
- *
+ *   
  * Created:  23 December 2009
- * Author:   Brian D. Wendt
- *   (http://principialabs.com/)
+ * Author:   Brian D. Wendt (http://principialabs.com/)
+ * 
+ * Modified:    Fall/Spring 2015/16
+ * Modified by: Ibrahim Ahmed (http://iahmed.me)
+ *   
  * Version:  1.1
  * License:  GPLv3
  *   (http://www.fsf.org/licensing/)
@@ -27,17 +28,8 @@
 // Import the Arduino Servo library
 #include <Servo.h> 
 
-// Create a Servo object for each servo
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
-Servo servo5;
-Servo servo6;
-
-// TO ADD SERVOS:
-//   Servo servo5;
-//   etc...
+// Create array of servos
+Servo servos[6];
 
 // Common servo setup values
 int minPulse = 600;   // minimum servo position, us (microseconds)
@@ -45,7 +37,8 @@ int maxPulse = 2400;  // maximum servo position, us
 
 // User input for servo and position
 int userInput[4];    // raw input from serial buffer, 4 bytes
-int startbyte;       // start byte, begin reading input
+char startbyte;       // start byte, begin reading input
+char mode;            // 'a' for angle, 'm' for pulse width in us
 int servo;           // which servo to pulse?
 int pos;             // servo angle 0-180
 int i;               // iterator
@@ -57,15 +50,13 @@ int pinState = LOW;
 void setup() 
 { 
   // Attach each Servo object to a digital pin
-  servo1.attach(9, minPulse, maxPulse);
-  servo2.attach(3, minPulse, maxPulse);
-  servo3.attach(4, minPulse, maxPulse);
-  servo4.attach(5, minPulse, maxPulse);
-  servo5.attach(10, minPulse, maxPulse);
-  servo6.attach(11, minPulse, maxPulse);
-  // TO ADD SERVOS:
-  //   servo5.attach(YOUR_PIN, minPulse, maxPulse);
-  //   etc...
+  servos[0].attach(9, minPulse, maxPulse);
+  servos[1].attach(3, minPulse, maxPulse);
+  servos[2].attach(4, minPulse, maxPulse);
+  servos[3].attach(5, minPulse, maxPulse);
+  servos[4].attach(10, minPulse, maxPulse);
+  servos[5].attach(11, minPulse, maxPulse);
+  
 
   // LED on Pin 13 for digital on/off demo
   pinMode(ledPin, OUTPUT);
@@ -83,48 +74,34 @@ void loop()
   if (Serial.available()>0) {
     // Read the first byte
     startbyte = Serial.read();
-    if (startbyte == 's') {
-      //Serial.println("Received");
-      servo = Serial.parseInt();
-      //Serial.print("Servo is: ");
-      //Serial.println(String(servo));
-      Serial.read(); //separating character b/w servo and angle
-      pos = Serial.parseInt();
-      //Serial.print("Angle is: ");
-      Serial.print("s");
-      Serial.print(String(servo));
-      Serial.print("a");
-      Serial.println(String(pos));
-      // Assign new position to appropriate servo
+    
+    if (startbyte == 's') {        // command mode
       
-      switch (servo) {
-        case 1:
-          servo1.write(pos);    // move servo1 to 'pos'
-          break;
-        case 2:
-          servo2.write(pos);
-          break;
-        case 3:
-          servo3.write(pos);
-          break;
-        case 4:
-          servo4.write(pos);
-          break;
-        case 5:
-          servo5.write(pos);
-          break;
-        case 6:
-          servo6.write(pos);
-          break;
+      servo = Serial.parseInt();
+      mode = Serial.read();
+      pos = Serial.parseInt();
+      Serial.print('s');
+      Serial.print(String(servo));
+      Serial.print(mode);
+      
+      if (mode == 'a') {           // move by angle
+        
+        // NOTE: from servo.h, angles have to be < 200 otherwise
+        // interpreted as pulse width.
+        servos[servo-1].write(pos);
+        Serial.println(servos[servo-1].read());  // returns angle 0-180
+        
+      } 
+      
+      else if (mode == 'm') {    // move by pulse width in us
+        
+        servos[servo-1].writeMicroseconds(pos);
+        Serial.println(servos[servo-1].readMicroseconds());  // read pulse width in us
+        
       }
-
-   // TO ADD SERVOS:
-   //     case 5:
-   //       servo5.write(pos);
-   //       break;
-   // etc...
     }
-    else if (startbyte == 'q') {
+    
+    else if (startbyte == 'q') {    // signal end
         Serial.println("Ending serial.");
         Serial.end();
     }
