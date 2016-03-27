44ff5568a0a3844b08726c9ec768f01ed5a0ae81
hazrmard <ibrahim78786@gmail.com>
2016-03-26 21:43:54 -0500

    simple modifications

--- trial.py
+++ trial.py
@@ -11,7 +11,7 @@ def altmap(x):
         return 130
     else:
         return 130. - x*(130./82.)
-        
+
 def altmap2(x):
     if x >= 90:
         return 40
@@ -26,10 +26,10 @@ ALTMAP = altmap2
 AZMAP = lambda x: 140.0 - (140.0/360.0)*x
 INTERVAL = 1
 MOTORS = (1,2)
-TLEPATH = 'fox1.tle'
+TLEPATH = 'AO-85.tle'
 PORT = 'COM3'
 PWM = (900, 2100)
-TRACE = 20
+TRACE = 0
 TIMEOUT = 2
 HOST = '0.0.0.0'
 
@@ -38,18 +38,18 @@ HOST = '0.0.0.0'
 def main():
 
     s = SatTrack()
-    
+
     print 'Setting location.'
     s.set_location()
-    
+
     print 'Loading TLE data.'
     s.load_tle(TLEPATH)
     print 'Satellite: ', s.id
-    
+
     print 'Starting computing at interval: ' + str(INTERVAL) + 's.'
     s.begin_computing(interval=INTERVAL, trace=TRACE)
     s.visualize(openbrowser=False, host=HOST)
-    
+
     print 'Connecting to servo motors.'
     s.connect_servos(port=PORT, minrange=(ALTRANGE[0], AZRANGE[0]), maxrange=(ALTRANGE[1], AZRANGE[1]), \
                                 pwm=PWM, map=(ALTMAP, AZMAP), timeout=TIMEOUT)
@@ -57,14 +57,14 @@ def main():
     print 'az.map(0):' + str(s.azmotor.map(0)), 'az.map(360):' + str(s.azmotor.map(360))
     print 'alt.map(0): ' + str(s.altmotor.map(0)), 'alt.map(90): ' + str(s.altmotor.map(90))
     print 'Finished connecting servos. Angle mapping complete.'
-    
+
     i='n'
     while not i=='y':
         i = raw_input('Start tracking? [y/n]   ')
         if i =='n': break
     if i=='y':
         s.begin_tracking()
-    
+
     raw_input('Press return to exit...')
     try:
         s.stop()
@@ -75,7 +75,7 @@ def main():
         s.server.stop_server()
     except:
         pass
-    
+
 
 
 if __name__=='__main__':
