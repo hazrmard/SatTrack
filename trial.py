0571df8b98500b213c82e438a4e74579b2f79a67
hazrmard <ibrahim78786@gmail.com>
2016-04-04 23:47:23 -0500

    changed tle file name + minor corrections

--- trial.py
+++ trial.py
@@ -11,7 +11,7 @@ def altmap(x):
         return 130
     else:
         return 130. - x*(130./82.)
-        
+
 def altmap2(x):
     if x >= 90:
         return 40
@@ -22,11 +22,11 @@ def altmap2(x):
 
 def altmap3(x):
     if x >= 90:
-        return 20
+        return 180
     elif x<=0:
-        return 155
+        return 60
     else:
-        return 155. - x*(135./90.)
+        return 60. + (120./(90.))*(x+0.)
 
 ALTRANGE = (0,90)
 AZRANGE = (0, 360)
@@ -34,7 +34,7 @@ ALTMAP = altmap3
 AZMAP = lambda x: 140.0 - (140.0/360.0)*x
 INTERVAL = 1
 MOTORS = (1,2)
-TLEPATH = 'fox1.tle'
+TLEPATH = 'AO-85.tle'
 PORT = 'COM3'
 PWM = (900, 2100)
 TRACE = 0
@@ -46,53 +46,53 @@ HOST = '0.0.0.0'
 def main():
 
     s = SatTrack()
-    
+
     print 'Setting location.'
     s.set_location()
-    
+
     print 'Loading TLE data.'
     s.load_tle(TLEPATH)
     print 'Satellite: ', s.id
-    
+
     print 'Starting computing at interval: ' + str(INTERVAL) + 's.'
     s.begin_computing(interval=INTERVAL, trace=TRACE)
     s.visualize(openbrowser=False, host=HOST)
-    
+
     print 'Connecting to servo motors.'
     s.connect_servos(port=PORT, minrange=(ALTRANGE[0], AZRANGE[0]), maxrange=(ALTRANGE[1], AZRANGE[1]), \
                                 pwm=PWM, map=(ALTMAP, AZMAP), timeout=TIMEOUT)
     print 'Test angle mapping: '
     print 'az.map(0):' + str(s.azmotor.map(0)), '\taz.map(360):' + str(s.azmotor.map(360))
     print 'alt.map(0): ' + str(s.altmotor.map(0)), '\talt.map(90): ' + str(s.altmotor.map(90))
-    
+
     i = raw_input('Test calibration?[y/n]   ')
     if i=='y':
         print 'Moving azimuth to ' + str(AZRANGE[1])
         s.azmotor.move(AZRANGE[1])
         _ = raw_input('Press return to continue...');
-        
+
         print 'Moving azimuth to ' + str(AZRANGE[0])
         s.azmotor.move(AZRANGE[0])
         _ = raw_input('Press return to continue...');
-        
+
         print 'Moving altitude to ' + str(ALTRANGE[1])
         s.altmotor.move(ALTRANGE[1])
         _ = raw_input('Press return to continue...');
-        
+
         print 'Moving altitude to ' + str(ALTRANGE[0])
         s.altmotor.move(ALTRANGE[0])
         _ = raw_input('Press return to continue...');
-    
+
     print 'Finished connecting servos. Angle mapping complete.'
     print 'Position at azimuth: ' + str(AZRANGE[0]) + '; position at altitude: ' + str(ALTRANGE[0])
-    
+
     i='n'
     while not i=='y':
         i = raw_input('Start tracking? [y/n]   ')
         if i =='n': break
     if i=='y':
         s.begin_tracking()
-    
+
     raw_input('Press return to exit...')
     try:
         s.stop()
@@ -103,7 +103,7 @@ def main():
         s.server.stop_server()
     except:
         pass
-    
+
 
 
 if __name__=='__main__':
