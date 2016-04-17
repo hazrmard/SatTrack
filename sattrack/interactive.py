614c7d84a332da61fa5bffc3b6d4c0cb886eae4b
hazrmard <ibrahim78786@gmail.com>
2016-04-17 15:43:30 -0500

    interactive mode broadcasts on network

--- sattrack/interactive.py
+++ sattrack/interactive.py
@@ -1,5 +1,5 @@
 from sattrack import *
 
 if __name__ == '__main__':
-   interface.Server().start_server()
+   interface.Server().start_server(host='0.0.0.0')
    raw_input('Interactive mode. Press enter to exit:  ')
